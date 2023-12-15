9678b78d1f45bb8472a3f16d4e61bdabd1044ffb
Matthew Yodhes <myodhes@umich.edu>
2023-12-15 12:10:57 -0500

    Created menu with input Validation

--- menu.py
+++ menu.py
@@ -1,9 +1,9 @@
 import dropbox
-from rich.prompt import Prompt
 from rich import print
-
 import tkinter as tk
 from tkinter import filedialog
+
+import stone
 
 class UserInputField:
     def __init__(self, root, label_text, example_text, save_variable,execute_function=None):
@@ -98,34 +98,39 @@ def verify_and_run(*args):
     json_tree = json_loader.save_variable[0]
     download_location = loc_loader.save_variable[0]
     access_token = api_entry.save_variable.get()
-    user_input2 = entry2.save_variable.get()
+    box_path = box_path_loader.save_variable.get()
 
-    print(f"User Input 1: {access_token}")
-    print(f"User Input 2: {user_input2}")
-    print(f"Path 1: {json_tree}")
-    print(f"Path 2: {download_location}")
+    print(f"API Token: {access_token}")
+    print(f"Dropbox Path: {box_path}")
+    print(f"JSON File: {json_tree}")
+    print(f"Download Path: {download_location}")
 
     # If user has entered input and selected paths
-    if all([access_token, user_input2, download_location]):
+    
+    if all([access_token, box_path, download_location]):
         # Close the window
-        root.destroy()
+        if authenticate(access_token,message=False) and validate_path(box_path,message=False):
+            root.destroy()
 
         # Run your custom function
         custom_function()
     else:
         # Show an error message
-        tk.messagebox.showerror("Error", "Please select download paths and enter user inputs.")
+        tk.messagebox.showerror("Error", "Please fill in all required fields.")
 
 def custom_function():
     # Replace this with your actual function logic
     print("Running custom function!")
 
-def authenticate(access_token):
+def authenticate(access_token, message=True):
     try:
         dbx = dropbox.Dropbox(access_token)
         account_info = dbx.users_get_current_account()
         user_name = account_info.name.display_name
-        tk.messagebox.showinfo("User Authenticated", f"Welcome {user_name}.")
+        if message == True:
+            tk.messagebox.showinfo("User Authenticated", f"Welcome {user_name}.")
+        
+        return access_token
     except dropbox.exceptions.AuthError as e:
         tk.messagebox.showerror("Error", f"Authentication error: Bad or expried token.\nPlease generate a new token or try again.")
     except dropbox.exceptions.InternalServerError as e:
@@ -133,6 +138,49 @@ def authenticate(access_token):
 
         return None
 
+def validate_path(path, message=True):
+    try:
+        # Create a Dropbox client
+        access_token = api_entry.save_variable.get()
+        print(access_token)
+        dbx = dropbox.Dropbox(access_token)
+
+        # Get metadata for the specified path
+        if dbx.files_get_metadata(path) and message==True:
+            tk.messagebox.showinfo("Valid path", f"Running this script will download all folders and subfolders in {path}.")
+            return True
+
+    except dropbox.exceptions.HttpError as e:
+            print(e)
+            print(type(e))
+          
+            if isinstance(e, dropbox.exceptions.BadInputError) and message==True:
+                 tk.messagebox.showinfo("Valid path", f"Note: running from root will download your entire Dropbox.")
+            return True
+
+    except (dropbox.exceptions.AuthError, dropbox.dropbox_client.BadInputException) as err:
+        # Authentication failed
+        tk.messagebox.showerror("Authentication Error", "Please enter a Valid API Token")
+
+        return False
+    except dropbox.exceptions.ApiError as e:
+        # Check if it's a GetMetadataError with a LookupError indicating 'not_found'
+        print(e.error.get_path())
+        print(type(e.error))
+        print(type(e.error.get_path))
+        if isinstance(e.error, dropbox.files.GetMetadataError):
+            print("create windows")
+            tk.messagebox.showerror("Path Not Found", "This path does not exist in your Dropbox. Please enter a valid path in the correct format.")
+    except stone.backends.python_rsrc.stone_validators.ValidationError as e:
+        tk.messagebox.showerror("Bad Path", "Bad format. Please enter a valid path in the correct format.\nHint: all paths should start with /")
+        
+    '''
+    except DropboxException as err:
+        # Other Dropbox-related exceptions
+        print("Dropbox error:", err)
+    
+        return False
+    '''
     
 
 # Create the main window
@@ -153,21 +201,21 @@ root.geometry(f"{window_width}x{window_height}+{x_position}+{y_position}")
 
 # Placeholder variables for entry input
 access_token = tk.StringVar()
-user_input2 = tk.StringVar()
+box_path = tk.StringVar()
 
 # Placeholder variables for file paths
 json_tree = [""]
 download_location = [""]
 
 # Create instances of the UserInputField class
-json_loader = PathSelector(root, "(optional) Load File Tree .json File", json_tree,select_file=True,filetypes=[("Text files", "*.txt"), ("All files", "*.*")])
+json_loader = PathSelector(root, "(optional) Load File Tree .json File", json_tree,select_file=True,filetypes=[("JSON files", "*.json"), ("All files", "*.*")])
 api_entry = UserInputField(root, "Dropbox API Token", "Paste Access Token Here", access_token,authenticate)
 
-entry2 = UserInputField(root, "User Input 2", "Enter your input here", user_input2) # Dropbox path
+box_path_loader = UserInputField(root, "Dropbox Path", "/Example/Path", box_path,validate_path) # Dropbox path
 
 # Create instances of the PathSelector class for download paths
 
-loc_loader = PathSelector(root, "Download Path 2", download_location)
+loc_loader = PathSelector(root, "Download Path", download_location)
 
 # Create a frame for the verification and run button
 frame_bottom = tk.Frame(root)
